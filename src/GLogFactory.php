21cd92edcc932331a6bdb8ce651beaca201a7f03
mdabagh <mohammadsajaddabbagh@gmail.com>
2023-05-02 16:47:31 +0330

    clean

--- src/GLogFactory.php
+++ src/GLogFactory.php
@@ -4,9 +4,14 @@ use Mdabagh\GenerateLog\classes\db;
 use Mdabagh\GenerateLog\classes\dbAndStorage;
 use Mdabagh\GenerateLog\classes\storage;
 
+interface LogStrategy
+{
+    public function make($type, $message, $data, $exception, $request, $response);
+}
+
 class GLogFactory
 {
-    public $state_active;
+    protected $state_active;
 
     public function __construct()
     {
@@ -15,98 +20,25 @@ class GLogFactory
 
     public function make($type, $message, $data, $exception, $request, $response)
     {
-        switch ($type) {
-            case 'INFO':
-                $log_type = config('GLog.log_type_active.INFO');
-                if ($log_type) {
-                    switch ($state_active) {
-                        case 'db':
-                            $db = new db();
-                            $db->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'storage':
-                            $storage = new storage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'dbAndStorage':
-                            $storage = new dbAndStorage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        default:
-                            # code...
-                            break;
-                    }
-                }
-                break;
-            case 'WARNING':
-                $log_type = config('GLog.log_type_active.WARNING');
-                if ($log_type) {
-                    switch ($state_active) {
-                        case 'db':
-                            $db = new db();
-                            $db->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'storage':
-                            $storage = new storage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'dbAndStorage':
-                            $storage = new dbAndStorage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        default:
-                            # code...
-                            break;
-                    }
-                }
-                break;
-            case 'DEBUG':
-                $log_type = config('GLog.log_type_active.DEBUG');
-                if ($log_type) {
-                    switch ($state_active) {
-                        case 'db':
-                            $db = new db();
-                            $db->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'storage':
-                            $storage = new storage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'dbAndStorage':
-                            $storage = new dbAndStorage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        default:
-                            # code...
-                            break;
-                    }
-                }
-                break;
-            case 'ERROR':
-                $log_type = config('GLog.log_type_active.ERROR');
-                if ($log_type) {
-                    switch ($state_active) {
-                        case 'db':
-                            $db = new db();
-                            $db->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'storage':
-                            $storage = new storage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        case 'dbAndStorage':
-                            $storage = new dbAndStorage();
-                            $storage->make($type, $message, $data, $exception, $request, $response);
-                            break;
-                        default:
-                            # code...
-                            break;
-                    }
-                }
-                break;
+        $log_type = config("GLog.log_type_active.{$type}");
 
+        if ($log_type) {
+            $strategy = $this->getStrategy();
+            $strategy->make($type, $message, $data, $exception, $request, $response);
+        }
+    }
+
+    protected function getStrategy(): LogStrategy
+    {
+        switch ($this->state_active) {
+            case 'db':
+                return new db();
+            case 'storage':
+                return new storage();
+            case 'dbAndStorage':
+                return new dbAndStorage();
             default:
-                break;
+                throw new InvalidArgumentException("Invalid state_active value: {$this->state_active}");
         }
     }
 }
